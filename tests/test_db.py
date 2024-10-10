a9f3d740069833fa25c01f02667072797cfe196d
Michael Dereszynski <mlderes@hotmail.com>
2024-10-10 13:16:18 -0500

    Modifications for all the User tests

--- tests/test_db.py
+++ tests/test_db.py
@@ -3,13 +3,16 @@ import api.services as services
 from api.models import User, Rating, Movie  
 
 class TestDB(unittest.TestCase):
+
+    def setUp(self) -> None:
+        return super().setUp()
+
     def test_create_connection(self):
         conn = services.get_db_connection()
         self.assertIsNotNone(conn)
         conn.close()
 
-    def setUp(self) -> None:
-        return super().setUp()
+class TestUserDB(unittest.TestCase):
 
     def test_get_all_users(self):
         all_users = services.get_all_users()
@@ -41,6 +44,16 @@ class TestDB(unittest.TestCase):
         for user in users:
             self.assertTrue("wilson" in user.user_name.lower())
 
+    def setup_known_user(self) -> User:
+        # Create a known user for testing purposes
+        known_user = User(None, "known_user", "knownuser@example.com")
+        known_user.id = services.create_user(known_user)
+        return known_user
+
+    def teardown_known_user(self, user_id):
+        # Delete the known user after the test
+        services.delete_user(user_id)
+
     def test_create_user(self):
         new_user = User(None, "test_user", "testuser@example.com")
         new_user.id = services.create_user(new_user)
@@ -50,35 +63,29 @@ class TestDB(unittest.TestCase):
         self.assertEqual(user.user_name, new_user.user_name)
         self.assertEqual(user.email, new_user.email)
         services.delete_user(new_user.id)
-        
-    # # Test to create a user
-    # print("Create a user")
-    # print("-----------")
-    #
-    # create_user(new_user)
-    # added_user = get_users_by_name('test_user')[0]
-    # print(added_user)
 
-    # # Test to update a user
-    # print("Update a user")
-    # print("-----------")
-    # # Start by getting the user to update
-    # user_to_update = get_users_by_name('test_user')[0]
-    # print(user_to_update)
-    # # Update the user
-    # user_to_update.user_name = 'updated_user'
-    # update_user(user_to_update)
-    # # Get the user again to see the changes
-    # updated_user = get_user_by_id(user_to_update.id)
-    # print(updated_user)
+    def test_update_user(self):
+        # Create a new user
+        known_user = self.setup_known_user()
 
-    # # # Test to remove a user
-    # print('Now delete the user')
-    # print("-----------")
-    # # Start by getting the last user put into the database
-    # last_user = get_all_users()[-1]
-    # delete_user(last_user.id)
-    # print(get_users_by_name(last_user.user_name))
+        # Update the user
+        known_user.user_name = "updated_user"
+        services.update_user(known_user)
+        updated_user = services.get_user_by_id(known_user.id)
+        self.assertEqual(updated_user.user_name, "updated_user")
 
+        # Delete the user
+        self.teardown_known_user(known_user.id)
+    
+    def test_delete_user(self):
+        # Create a new user
+        known_user = self.setup_known_user()
+
+        # Delete the user
+        services.delete_user(known_user.id)
+        deleted_user = services.get_user_by_id(known_user.id)
+        self.assertIsNone(deleted_user)
+
+    
 if __name__ == '__main__':
     unittest.main()
