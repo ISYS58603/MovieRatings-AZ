a8e9e635d974ee4d682484509fe5e1b24b8c6fa1
Michael Dereszynski <mlderes@hotmail.com>
2024-10-15 12:49:02 -0500

    Make the change to known_user, known_movie

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,6 @@
 import pytest
 from run import create_app
-from api.models import User, Rating, Movie
+from api.models import User, Rating, Movie, create_user_from_dict
 from flask import json
 
 
@@ -19,14 +19,14 @@ def known_user(test_client):
     user_data = {"username": "known_user", "email": "knownuser@example.com"}
     response = test_client.post("/api/users", json=user_data)
     data = json.loads(response.data)
-    user_id = data["user"]["id"]
-    yield user_id
+    new_user = create_user_from_dict(data["user"])
+    yield new_user
     # Delete the known user after the test
-    test_client.delete(f"/api/users/{user_id}")
+    test_client.delete(f"/api/users/{new_user.id}")
 
 
 @pytest.fixture
-def new_movie(test_client):
+def known_movie(test_client):
     # Create a new movie for testing purposes via API
     movie_data = {
         "title": "test_movie",
@@ -42,7 +42,7 @@ def new_movie(test_client):
     test_client.delete(f"/api/movies/{new_movie.movie_id}")
 
 
-class TestDatabaseConnections:
+class TestDatabaseConnection:
     def test_create_connection(self, test_client):
         response = test_client.get("/api/connection")
         assert response.status_code == 200
@@ -61,43 +61,37 @@ class TestUserRoutes:
         data = json.loads(response.data)
         assert len(data) > 0
 
-    def test_get_user_by_id(self, test_client):
-        response = test_client.get("/api/users/1")
+    def test_get_user_by_id(self, test_client, known_user):
+        response = test_client.get(f"/api/users/{known_user.id}")
         assert response.status_code == 200
         user = json.loads(response.data)
         assert user is not None
-        assert user["id"] == 1
-        assert user["username"] == "jane_doe"
+        assert user["id"] == known_user.id
+        assert user["username"] == known_user.username
 
-    def test_get_users_by_starts_with_name(self, test_client):
-        response = test_client.get("/api/users?starts_with=jane")
+    def test_get_users_by_starts_with_name(self, test_client, known_user):
+        partial_name = known_user.username[:3]
+        response = test_client.get(f"/api/users?starts_with={partial_name}")
         assert response.status_code == 200
         users = json.loads(response.data)
         assert len(users) > 0
         for user in users:
-            assert "jane" in user["username"].lower()
+            assert partial_name in user["username"].lower()
 
         # Now test for the opposite case, where we shouldn't get the user back
-        response = test_client.get("/api/users?starts_with=wilson")
+        response = test_client.get("/api/users?starts_with=xxxy")
         assert response.status_code == 200
         users = json.loads(response.data)
         assert len(users) == 0
 
-    def test_get_users_by_contains_name(self, test_client):
-        response = test_client.get("/api/users?contains=jane")
+    def test_get_users_by_contains_name(self, test_client, known_user):
+        partial_name = known_user.username[3:]
+        response = test_client.get(f"/api/users?contains={partial_name}")
         assert response.status_code == 200
         users = json.loads(response.data)
         assert len(users) > 0
         for user in users:
-            assert "jane" in user["username"].lower()
-
-        # Now test for a user where just a part of the name is in the username
-        response = test_client.get("/api/users?contains=wilson")
-        assert response.status_code == 200
-        users = json.loads(response.data)
-        assert len(users) > 0
-        for user in users:
-            assert "wilson" in user["username"].lower()
+            assert partial_name in user["username"].lower()
 
     def test_create_user(self, test_client):
         user_data = {"username": "test_user", "email": "testuser@example.com"}
@@ -117,21 +111,21 @@ class TestUserRoutes:
     def test_update_user(self, test_client, known_user):
         # Update the user
         updated_data = {"username": "updated_user", "email": "knownuser@example.com"}
-        response = test_client.put(f"/api/users/{known_user}", json=updated_data)
+        response = test_client.put(f"/api/users/{known_user.id}", json=updated_data)
         assert response.status_code == 200
 
         # Verify update
-        response = test_client.get(f"/api/users/{known_user}")
+        response = test_client.get(f"/api/users/{known_user.id}")
         updated_user = json.loads(response.data)
         assert updated_user["username"] == "updated_user"
 
     def test_delete_user(self, test_client, known_user):
         # Delete the user
-        response = test_client.delete(f"/api/users/{known_user}")
+        response = test_client.delete(f"/api/users/{known_user.id}")
         assert response.status_code == 200
 
         # Verify deletion
-        response = test_client.get(f"/api/users/{known_user}")
+        response = test_client.get(f"/api/users/{known_user.id}")
         assert response.status_code == 404
 
 
@@ -142,13 +136,13 @@ class TestMovieRoutes:
         data = json.loads(response.data)
         assert len(data) > 0
 
-    def test_get_movie_by_id(self, test_client,new_movie):
-        response = test_client.get(f"/api/movies/{new_movie.movie_id}")
+    def test_get_movie_by_id(self, test_client,known_movie):
+        response = test_client.get(f"/api/movies/{known_movie.movie_id}")
         assert response.status_code == 200
         movie = json.loads(response.data)
         assert movie is not None
-        assert movie["movie_id"] == new_movie.movie_id
-        assert movie["title"] == new_movie.title
+        assert movie["movie_id"] == known_movie.movie_id
+        assert movie["title"] == known_movie.title
         
     def test_create_movie(self, test_client):
         movie_data = {
@@ -172,16 +166,16 @@ class TestMovieRoutes:
         assert movie["director"] == movie_data["director"]
         test_client.delete(f"/api/movies/{movie_id}")
 
-    def test_delete_movie(self, test_client, new_movie):
+    def test_delete_movie(self, test_client, known_movie):
         # Delete the movie
-        response = test_client.delete(f"/api/movies/{new_movie.movie_id}")
+        response = test_client.delete(f"/api/movies/{known_movie.movie_id}")
         assert response.status_code == 200
 
         # Verify deletion
-        response = test_client.get(f"/api/movies/{new_movie.movie_id}")
+        response = test_client.get(f"/api/movies/{known_movie.movie_id}")
         assert response.status_code == 404
 
-    def test_update_movie(self, test_client, new_movie):
+    def test_update_movie(self, test_client, known_movie):
         # Update the movie
         updated_data = {
             "title": "updated_movie",
@@ -189,10 +183,14 @@ class TestMovieRoutes:
             "release_year": 2024,
             "director": "Test Director",
         }
-        response = test_client.put(f"/api/movies/{new_movie.movie_id}", json=updated_data)
+        response = test_client.put(f"/api/movies/{known_movie.movie_id}", json=updated_data)
         assert response.status_code == 200
 
         # Verify update
-        response = test_client.get(f"/api/movies/{new_movie.movie_id}")
+        response = test_client.get(f"/api/movies/{known_movie.movie_id}")
         updated_movie = json.loads(response.data)
         assert updated_movie["title"] == "updated_movie"
+
+class TestReviewRoutes:
+    
+    pass
